9a9e26f25528ac0e6aff7f426880a98adc9d07d0
Moetez <113128080+EMoetez@users.noreply.github.com>
2025-05-27 11:05:04 +0100

    Update start-stack.sh

--- start-stack.sh
+++ start-stack.sh
@@ -29,6 +29,13 @@ else
 fi
 echo "--------------------------------------------------"
 
+# Download the specific version
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64" -o /usr/local/bin/docker-compose
+
+# Make it executable
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-composedocker-compose version
 
 # --- 2. Start Elasticsearch and Wait for it to be Healthy ---
 echo "STARTUP: Bringing up ${ELASTICSEARCH_SERVICE_NAME} service..."
